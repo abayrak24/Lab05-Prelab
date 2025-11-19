686d7335e84a167c0f7bcf697fde54f41c3cced3
abayrak24 <abayrak24@ku.edu.tr>
2025-11-19 18:50:12 +0300

    prelab5

--- q1.py
+++ q1.py
@@ -2,4 +2,12 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    n = len(s)
+    best = ""
+    
+    for i in range(n):
+        for j in range(i+2,n+1):
+            piece = s[i:j]
+            if piece == piece[::-1] and len(piece) > len(best):
+                best = piece
+    return best
